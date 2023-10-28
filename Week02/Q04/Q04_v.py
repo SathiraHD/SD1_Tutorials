4e79cc0512a68ccc2dd07c5c7429a4473ea290e9
Sathira Dahanayaka <149231808+SathiraHD@users.noreply.github.com>
2023-10-29 00:12:38 +0530

    Update Q04_v.py

--- Week02/Q04/Q04_v.py
+++ Week02/Q04/Q04_v.py
@@ -4,3 +4,18 @@ num_1 = input('Enter number one: ')
 num_2 = input('Enter number two: ')
 total = (int(num_1) + int(num_2))
 print(total)
+
+
+print('Convert str to float')
+cost = input('Enter cost of items: ')
+paid = input('Enter cash paid: ')
+change = (float(paid) - float(cost))
+print('Change is: ', change)
+
+
+print('calculate the average')
+num_1 = int(input('Enter the first number: '))
+num_2 = int(input('Enter the second number: '))
+num_3 = int(input('Enter the third number: '))
+average = (num_1 + num_2 + num_3)/3
+print('Averge is: ', round(average,3))
