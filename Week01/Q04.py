96a8640f947445a8f9df978a2aaa8d087ece5c52
Sathira Dahanayaka <149231808+SathiraHD@users.noreply.github.com>
2023-10-28 23:42:33 +0530

    Q04.py

---
+++ Week01/Q04.py
@@ -0,0 +1,6 @@
+# Question 4
+num_1 = float(input("Enter first number: "))
+num_2 = float(input("Enter second number: "))
+num_3 = float(input("Enter third number: "))
+avg = round(((num_1 + num_2 + num_3)/3), 2)
+print("Average is: ", avg)
