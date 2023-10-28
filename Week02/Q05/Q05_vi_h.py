80b131158be7b851dadb0cd2b843087e5d69cf3f
Sathira Dahanayaka <149231808+SathiraHD@users.noreply.github.com>
2023-10-29 00:08:53 +0530

    Create Q05_vi_h.py

---
+++ Week02/Q05/Q05_vi_h.py
@@ -0,0 +1,6 @@
+# Q05 vi) h)
+len = float(input('Enter the length of the box: '))
+height = float(input('Enter the height of the box'))
+width = float(input('Enter the width of the box'))
+volume = len * height * width
+print('The volume of the box is', volume)
