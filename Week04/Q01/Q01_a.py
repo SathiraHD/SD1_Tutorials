da065f0256970db02fc16a158d5782a71c82a9b7
Sathira Dahanayaka <149231808+SathiraHD@users.noreply.github.com>
2023-10-29 01:23:48 +0530

    Create Q01_a.py

---
+++ Week04/Q01/Q01_a.py
@@ -0,0 +1,9 @@
+#initialize the variables
+hidden = 6
+guess = int(input('Enter guess number between 1 to 20 : '))
+while (guess != hidden):
+    print('Your guess is not correnct, Try again!!')
+    guess = int(input('Enter guess number between 1 to 20 : '))
+    break
+print('Congratulation!! Your guess was correct')
+
