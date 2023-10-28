db7c5096c138ca88635131f4ee5882321438c3e7
Sathira Dahanayaka <149231808+SathiraHD@users.noreply.github.com>
2023-10-29 01:44:20 +0530

    Delete Week01 directory

--- Week01/Q02.py
+++
@@ -1,5 +0,0 @@
-# Question 2
-num_1 = int(input("Enter first number : "))
-num_2 = int(input("Enter second number : "))
-total = num_1 + num_2
-print(total)
--- Week01/Q03.py
+++
@@ -1,5 +0,0 @@
-# Question 3
-cost = float(input("Enter cost of item: "))
-paid = float(input("Enter cash paid: "))
-change = paid - cost
-print("Change is: ", change)
